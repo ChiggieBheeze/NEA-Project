ddb5d038107193761bde32f6160adb55dfeb0b91
ben :3 <90383326+ChiggieBheeze@users.noreply.github.com>
2023-01-30 14:03:22 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,44 @@
+#Screen size
 w_Width = 640
 w_Height = 480
 
-tile_size = 32
+#Frames per second
+FPS = 60
 
-p_Layer = 1
+#Size of tiles
+tile_size = 32
+tile_size2 = 16
+
+#Colours
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+#Layer of sprites
+p_Layer = 3
+w_LAYER = 2
+g_Layer = 1
+
+#speed of the player
+p_Speed = 1
+
+#The code for the layout of the tilemap
+TileMap = [
+    'WWWWWWWWWWWWWWWWWWWW',
+    'W..................W',
+    'W..................W',
+    'W....P.............W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'W..................W',
+    'WWWWWWWWWWWWWWWWWWWW',
+    #....................
+]
